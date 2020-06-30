d25194820c56db80c7899e7bb5311e6264945b8c
jychoi927 <jychoi927@gmail.com>
2020-06-30 12:49:32 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://cjy:1234@boilerplate.mrvqv.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log("MongoDB connected.."))
+  .catch(err => console.log(err))
+
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
